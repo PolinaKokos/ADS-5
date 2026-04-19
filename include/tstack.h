bf2ceda96a293deca443e56bb9398634e9be3a8c
PolinaKokos <kosikpm@gmail.com>
2026-04-19 19:12:19 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,35 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
+#ifndef TSTACK_H
+#define TSTACK_H
+template <typename T, int N>
 class TStack {
-  // добавьте код стека
+private:
+    T data[N];
+    int topIndex;
+public:
+    TStack() : topIndex(-1) {}
+    void push(const T& value) {
+        if (topIndex < N - 1) {
+            data[++topIndex] = value;
+        }
+    }
+    T pop() {
+        if (topIndex >= 0) {
+            return data[topIndex--];
+        }
+        return T();
+    }
+    T top() const {
+        if (topIndex >= 0) {
+            return data[topIndex];
+        }
+        return T();
+    }
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+    int size() const {
+        return topIndex + 1;
+    }
 };
-
-#endif  // INCLUDE_TSTACK_H_
+#endif
